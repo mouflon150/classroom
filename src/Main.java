d042d18901a5c8a14e85c2d81eb30855556763b5
Marlen Temirbaev <mouflon150@gmail.com>
2022-12-13 18:05:41 +0600

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -19,3 +19,17 @@ public class Main {
         fileReader.close();
     }
 }
+class Exceptions {
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            int x = Integer.parseInt(scanner.nextLine());
+
+            if (x != 0) {
+                throw new IOException(); // Выбрасывание исключений
+            }
+        }
+    }
+}
--- src/enums/Main.java
+++ src/enums/Main.java
@@ -24,18 +24,15 @@ public class Main {
                     input *= Enums.DOLLAR.currency;
                     System.out.println("KGS: " + input + "\n");
                     break;
-                }
-                if (num == Enums.RUB.ordinal()) {
+                } else if (num == Enums.RUB.ordinal()) {
                     input *= Enums.RUB.currency;
                     System.out.println("KGS: " + input + "\n");
                     break;
-                }
-                if (num == Enums.EURO.ordinal()) {
+                } else if (num == Enums.EURO.ordinal()) {
                     input *= Enums.EURO.currency;
                     System.out.println("KGS: " + input + "\n");
                     break;
-                }
-                if (num == Enums.POUND.ordinal()) {
+                } else if (num == Enums.POUND.ordinal()) {
                     input *= Enums.POUND.currency;
                     System.out.println("KGS: " + input + "\n");
                     break;
